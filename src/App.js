dd9f9c00ad90f85fa61100caf346573155d497cf
John Franti <john.franti@flatironschool.com>
2019-06-28 11:40:14 -0700

    add cool stuff

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React, { Component } from "react";
-import Welcome from "./components/Welcome";
-import { Response } from "./components/Response";
-import Hobbit from "./components/Hobbit";
-import { Hobbits } from "./assets/Hobbits";
+import React, { Component, Fragment } from "react";
 import "./App.css";
+import Cat from "./components/Cat";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showResponse: false,
-      shire: Hobbits
-    };
-  }
-  sayHi = () => {
-    let newShire = this.state.shire;
-    newShire.push({
-      name: "Gandalf",
-      age: 2019,
-      personality: "noice"
-    });
-    this.setState(Object.assign({}, { showResponse: true }, { newShire }));
-  };
   render() {
     return (
-      <div id="welcome">
-        <Welcome clickAction={this.sayHi} guest="Mr. Frodo" />
-        <hr />
-        {this.state.showResponse ? <Response /> : null}
-        {this.state.shire.map((hobbit, index) => {
-          return <Hobbit hobbit={hobbit} key={index} />;
-        })}
-      </div>
+      <Fragment>
+        <Cat />
+      </Fragment>
     );
   }
 }
--- src/components/Response.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Response = () => {
-    return <h2>Where are all those carrots, then?</h2>
-}
